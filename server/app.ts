5184a5235ba29a57468cd05ce723c1052187231f
Yuebin <yuebin25@mit.edu>
2023-10-17 01:24:52 -0400

    changed server directory

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,11 @@
+import ChatConcept from "./concepts/chat";
+import CircleConcept from "./concepts/circle";
+import CommentConcept from "./concepts/comment";
+import FeedConcept from "./concepts/feed";
 import FriendConcept from "./concepts/friend";
 import PostConcept from "./concepts/post";
+import ProfileConcept from "./concepts/profile";
+import RecommendConcept from "./concepts/recommend";
 import UserConcept from "./concepts/user";
 import WebSessionConcept from "./concepts/websession";
 
@@ -8,3 +14,9 @@ export const WebSession = new WebSessionConcept();
 export const User = new UserConcept();
 export const Post = new PostConcept();
 export const Friend = new FriendConcept();
+export const Profile = new ProfileConcept();
+export const Circle = new CircleConcept();
+export const Chat = new ChatConcept();
+export const Comment = new CommentConcept();
+export const Feed = new FeedConcept();
+export const Recommend = new RecommendConcept();
--- server/routes.ts
+++ server/routes.ts
@@ -1,12 +1,13 @@
-import { ObjectId } from "mongodb";
+import { ObjectId, W } from "mongodb";
 
 import { Router, getExpressRouter } from "./framework/router";
 
-import { Friend, Post, User, WebSession } from "./app";
+import { Friend, Post, User, WebSession, Profile, Circle, Chat, Comment, Feed, Recommend} from "./app";
 import { PostDoc, PostOptions } from "./concepts/post";
 import { UserDoc } from "./concepts/user";
 import { WebSessionDoc } from "./concepts/websession";
 import Responses from "./responses";
+import { ContentT, ActionT } from "./types";
 
 class Routes {
   @Router.get("/session")
@@ -70,9 +71,9 @@ class Routes {
   }
 
   @Router.post("/posts")
-  async createPost(session: WebSessionDoc, content: string, options?: PostOptions) {
+  async createPost(session: WebSessionDoc, content: ContentT, viewers: Set<ObjectId>, options?: PostOptions) {
     const user = WebSession.getUser(session);
-    const created = await Post.create(user, content, options);
+    const created = await Post.create(user, content, viewers, options);
     return { msg: created.msg, post: await Responses.post(created.post) };
   }
 
@@ -90,6 +91,13 @@ class Routes {
     return Post.delete(_id);
   }
 
+  @Router.patch("posts/changeViewers/:_id")
+  async changePostViewers(session: WebSessionDoc, _id: ObjectId, viewers: Set<ObjectId>) {
+    const user = WebSession.getUser(session);
+    await Post.isAuthor(user, _id);
+    return Post.changeViewers(_id, viewers)
+  }
+
   @Router.get("/friends")
   async getFriends(session: WebSessionDoc) {
     const user = WebSession.getUser(session);
@@ -100,6 +108,10 @@ class Routes {
   async removeFriend(session: WebSessionDoc, friend: string) {
     const user = WebSession.getUser(session);
     const friendId = (await User.getUserByUsername(friend))._id;
+    const circle = await Circle.findFriend(user, friendId)
+    if (circle !== undefined) {
+      await Circle.removeFromCircle(circle._id, friendId);
+    }
     return await Friend.removeFriend(user, friendId);
   }
 
@@ -136,6 +148,194 @@ class Routes {
     const fromId = (await User.getUserByUsername(from))._id;
     return await Friend.rejectRequest(fromId, user);
   }
+
+  @Router.get("/profile/:owner")
+  async getProfile(session: WebSessionDoc, owner: ObjectId) {
+    const viewer = WebSession.getUser(session);
+    return await Profile.getProfile(owner, viewer);
+  }
+
+  @Router.post("/profile/create")
+  async createProfile(session: WebSessionDoc, name: string, content: ContentT) {
+    const user = WebSession.getUser(session);
+    return await Profile.createProfile(user, name, content);
+  }
+
+  @Router.patch("/profile/update/:_id")
+  async updateProfile(session: WebSessionDoc, _id: ObjectId, name: string, content: ContentT) {
+    const user = WebSession.getUser(session);
+    await Profile.isCreator(user, _id);
+    return await Profile.updateProfile(_id, name, content);
+  }
+
+  @Router.post("/profile/create_view/")
+  async createProfileView(session: WebSessionDoc, owner: ObjectId, name: string) {
+    const user = WebSession.getUser(session);
+    await Friend.isNotFriends(user, owner);
+    return await Profile.createProfileView(owner, user, name);
+  }
+
+  @Router.patch("/profile/change_view/:_id")
+  async changeProfileView(session: WebSessionDoc, _id: ObjectId, name: string) {
+    const user = WebSession.getUser(session);
+    await Friend.isNotFriends(user, await Profile.getCreator(_id));
+    return await Profile.changeProfileView(_id, name);
+  }
+
+  @Router.post("/circle/create")
+  async createCircle(session: WebSessionDoc, members: Set<ObjectId>, name: string, actions: Set<ActionT>) {
+    const user = WebSession.getUser(session);
+    return await Circle.createCircle(user, members, name, actions);
+  }
+
+  @Router.delete("/circle/delete/:_id")
+  async deleteCircle(session: WebSessionDoc, _id: ObjectId) {
+    const user = WebSession.getUser(session);
+    await Circle.isCreator(user, _id);
+    return await Circle.deleteCircle(_id);
+  }
+
+  @Router.get("/circles")
+  async getCircles(session: WebSessionDoc) {
+    const user = WebSession.getUser(session);
+    return await Circle.getCirclesByUser(user);
+  }
+
+  @Router.patch("/circle/add/:_id")
+  async addToCircle(session: WebSessionDoc, _id: ObjectId, member: ObjectId) {
+    const user = WebSession.getUser(session);
+    await Circle.isCreator(user, _id);
+    return await Circle.addToCircle(_id, member);
+  }
+
+  @Router.patch("/circle/remove/:_id")
+  async removeFromCircle(session: WebSessionDoc, _id: ObjectId, member: ObjectId) {
+    const user = WebSession.getUser(session);
+    await Circle.isCreator(user, _id);
+    return await Circle.removeFromCircle(_id, member);
+  }
+
+  @Router.patch("/circle/change/:_id")
+  async changeCircleActions(session: WebSessionDoc, _id: ObjectId, actions: Set<ActionT>) {
+    const user = WebSession.getUser(session);
+    await Circle.isCreator(user, _id);
+    return await Circle.changeActions(_id, actions);
+  }
+
+  @Router.get("/chat/:receiver")
+  async getChat(session: WebSessionDoc, receiver: ObjectId) {
+    const user = WebSession.getUser(session);
+    return await Chat.getChat(user, receiver);
+  }
+
+  @Router.post("/chat/create")
+  async createChat(session: WebSessionDoc, receiver: ObjectId, content: ContentT) {
+    const user = WebSession.getUser(session);
+    await Circle.hasAction(user, receiver, "Chat");
+    await Circle.hasAction(receiver, user, "Chat");
+    return await Chat.createChat(user, receiver, content);
+  }
+
+  @Router.patch("/chat/send/:receiver")
+  async sendMessage(session: WebSessionDoc, receiver: ObjectId, content: ContentT) {
+    const user = WebSession.getUser(session);
+    await Circle.hasAction(user, receiver, "Chat");
+    await Circle.hasAction(receiver, user, "Chat");
+    return await Chat.sendMessage(user, receiver, content);
+  }
+
+  @Router.get("/comments/:post")
+  async getComments(session: WebSessionDoc, post: ObjectId) {
+    const user = WebSession.getUser(session);
+    await Circle.hasAction(await Post.getAuthor(post), user, "ViewPost");
+    await Circle.hasAction(await Post.getAuthor(post), user, "ViewPostComments");
+    const comments = await Comment.getComments(post);
+    return comments.filter(comment => comment.viewers.has(user));
+  }
+
+  @Router.post("/comment/create")
+  async comment(session: WebSessionDoc, post: ObjectId, viewers: Set<ObjectId>, message: ContentT) {
+    const user = WebSession.getUser(session);
+    await Circle.hasAction(await Post.getAuthor(post), user, "Comment");
+    return await Comment.comment(user, viewers, post, message);
+  }
+
+  @Router.delete("/comment/delete/:_id")
+  async deleteComment(session: WebSessionDoc, _id: ObjectId) {
+    const user = WebSession.getUser(session);
+    await Comment.isAuthor(user, _id);
+    return await Comment.delComment(_id);
+  }
+
+  @Router.get("/feed/:owner")
+  async getFeed(session: WebSessionDoc, owner: ObjectId) {
+    const viewer = WebSession.getUser(session);
+    if (viewer.toString() === owner.toString()) {
+      return await Feed.getFeed(owner);
+    }
+    await Circle.hasAction(owner, viewer, "ViewFeed");
+    return await Feed.getFeed(owner);
+  }
+
+  @Router.post("/feed/create")
+  async createFeed(session: WebSessionDoc) {
+    const user = WebSession.getUser(session);
+    const viewers: Set<ObjectId> = new Set();
+    const posts: ObjectId[] = [];
+    for (const friend of await Friend.getFriends(user)) {
+      // only posts that person is allowed to see appears on their feed
+      if (await Circle.hasAction(friend, user, "ViewPost")) {
+        const friendPosts = await Post.getByAuthor(friend);
+        posts.concat(friendPosts.map(post => post._id));
+      }
+      // viewers who are allowed to see the person's feed
+      if (await Circle.hasAction(user, friend, "ViewFeed")) {
+        viewers.add(friend);
+      }
+    }
+    return await Feed.createFeed(user, viewers, posts, new Date());
+  }
+
+  @Router.patch("/feed/update")
+  async updateFeed(session: WebSessionDoc) {
+    const user = WebSession.getUser(session);
+    const posts: ObjectId[] = [];
+    for (const friend of await Friend.getFriends(user)) {
+      // only posts that person is allowed to see appears on their feed
+      if (await Circle.hasAction(friend, user, "ViewPost")) {
+        const friendPosts = await Post.getByAuthor(friend);
+        posts.concat(friendPosts.map(post => post._id));
+      }
+    }
+    return await Feed.updateFeed(user, posts, new Date());
+  }
+
+  @Router.patch("/feed/changeViewers")
+  async changeFeedViewers(session: WebSessionDoc, viewers: Set<ObjectId>) {
+    const user = WebSession.getUser(session);
+    return await Feed.changeViewers(user, viewers);
+  }
+
+  @Router.get("recommends")
+  async getRecs(session: WebSessionDoc) {
+    const user = WebSession.getUser(session);
+    return await Recommend.getRecsbyTarget(user);
+  }
+
+  @Router.post("/recommend/create/:post/:target")
+  async recommend(session: WebSessionDoc, post: ObjectId, target: ObjectId) {
+    const user = WebSession.getUser(session);
+    await Circle.hasAction(target, user, "Recommend");
+    return await Recommend.recommend(user, post, target);
+  }
+
+  @Router.delete("/recommend/delete/:_id")
+  async removeRec(session: WebSessionDoc, _id: ObjectId) {
+    const user = WebSession.getUser(session);
+    await Recommend.isRecommender(user, _id);
+    return await Recommend.removeRec(_id);
+  }
+
 }
 
 export default getExpressRouter(new Routes());
